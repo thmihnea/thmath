58a38f9ae8d60f776bcabd54de094cf4197b4200
Morărescu Mihnea <mihneathm@gmail.com>
2024-04-18 16:26:06 +0300

    added complex number support

---
+++ complex.cpp
@@ -0,0 +1,116 @@
+/*
+ * This file is part of thmath.
+ *
+ * Developed for the the thmath Mathematics Library.
+ * This product includes software developed by Mihnea Morarescu and
+ * all affiliated contributors of thmath.
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "complex.h"
+#include <cmath>
+
+thmath::Complex::Complex(double real, double imaginary)
+{
+    this->real = real;
+    this->imaginary = imaginary;
+}
+
+thmath::Complex::~Complex()
+{
+    
+}
+
+double thmath::Complex::get_real() const
+{
+    return this->real;
+}
+
+double thmath::Complex::get_imaginary() const
+{
+    return this->imaginary;
+}
+
+double thmath::Complex::norm() const
+{
+    return std::sqrt(
+        this->real * this->real + this->imaginary * this->imaginary
+    );
+}
+
+double thmath::Complex::argument() const
+{
+    return std::atan2(
+        this->imaginary,
+        this->real
+    );
+}
+
+thmath::Complex thmath::Complex::conjugate() const
+{
+    return thmath::Complex(
+        this->real,
+        -this->imaginary
+    );
+}
+
+bool thmath::Complex::operator==(const Complex& complex) const 
+{
+    return (this->real == complex.real) && (this->imaginary == complex.imaginary);
+}
+
+thmath::Complex thmath::Complex::operator+(const Complex& complex) const 
+{
+    return Complex(this->real + complex.real, this->imaginary + complex.imaginary);
+}
+
+
+thmath::Complex& thmath::Complex::operator+=(const Complex& complex) 
+{
+    this->real += complex.real;
+    this->imaginary += complex.imaginary;
+    return *this;
+}
+
+thmath::Complex thmath::Complex::operator-(const Complex& complex) const 
+{
+    return Complex(this->real - complex.real, this->imaginary - complex.imaginary);
+}
+
+thmath::Complex& thmath::Complex::operator-=(const Complex& complex)
+{
+    this->real -= complex.real;
+    this->imaginary -= complex.imaginary;
+    return *this;
+}
+
+thmath::Complex thmath::Complex::operator*(const Complex& complex) const 
+{
+    double result_real = this->real * complex.real - this->imaginary * complex.imaginary;
+    double result_imaginary = this->real * complex.imaginary + this->imaginary * complex.real;
+    return Complex(result_real, result_imaginary);
+}
+
+thmath::Complex& thmath::Complex::operator*=(const Complex& complex) 
+{
+    double result_real = this->real * complex.real - this->imaginary * complex.imaginary;
+    double result_imaginary = this->real * complex.imaginary + this->imaginary * complex.real;
+    real = result_real;
+    imaginary = result_imaginary;
+    return *this;
+}
+
---
+++ complex.h
@@ -0,0 +1,145 @@
+/*
+ * This file is part of thmath.
+ *
+ * Developed for the the thmath Mathematics Library.
+ * This product includes software developed by Mihnea Morarescu and
+ * all affiliated contributors of thmath.
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifndef __THMATH_COMPLEX_
+#define __THMATH_COMPLEX_
+
+namespace thmath
+{
+    class Complex
+    {
+    private:
+        double real;
+        double imaginary;
+
+    public:
+        /**
+         * Default constructor for the Complex class.
+         * 
+         * @param real The real part of the complex number.
+         * @param imaginary The imaginary part of the complex number.
+         * @return A new complex number object.
+        */
+        Complex(double real, double imaginary);
+
+        /**
+         * Destructor for the Complex class.
+        */
+        ~Complex();
+
+        /**
+         * Get the real part of the complex number.
+         * 
+         * @return The real part of the complex number.
+        */
+        double get_real() const;
+
+        /**
+         * Get the imaginary part of the complex number.
+         * 
+         * @return The imaginary part of the complex number.
+        */
+        double get_imaginary() const;
+
+        /**
+         * Calculate the norm (magnitude) of the complex number.
+         * 
+         * @return The norm of the complex number.
+        */
+        double norm() const;
+
+        /**
+         * Calculate the argument (angle) of the complex number.
+         * 
+         * @return The argument of the complex number.
+        */
+        double argument() const;
+
+        /**
+         * Calculate the conjugate of the complex number.
+         * 
+         * @return The conjugate of the complex number.
+        */
+        Complex conjugate() const;
+
+        /**
+         * Overloaded equality operator for comparing two complex numbers.
+         * 
+         * @param complex The complex number to compare with.
+         * @return True if the complex numbers are equal, false otherwise.
+        */
+        bool operator==(const Complex& complex) const;
+
+        /**
+         * Overloaded addition operator for adding two complex numbers.
+         * 
+         * @param complex The complex number to add.
+         * @return The result of the addition operation.
+        */
+        Complex operator+(const Complex& complex) const;
+
+        /**
+         * Overloaded compound addition operator for adding two complex numbers.
+         * 
+         * @param complex The complex number to add.
+         * @return The result of the addition operation.
+        */
+        Complex& operator+=(const Complex& complex);
+
+        /**
+         * Overloaded subtraction operator for subtracting two complex numbers.
+         * 
+         * @param complex The complex number to subtract.
+         * @return The result of the subtraction operation.
+        */
+        Complex operator-(const Complex& complex) const;
+
+        /**
+         * Overloaded compound subtraction operator for subtracting two complex numbers.
+         * 
+         * @param complex The complex number to subtract.
+         * @return The result of the subtraction operation.
+        */
+        Complex& operator-=(const Complex& complex);
+
+        /**
+         * Overloaded multiplication operator for multiplying two complex numbers.
+         * 
+         * @param complex The complex number to multiply.
+         * @return The result of the multiplication operation.
+        */
+        Complex operator*(const Complex& complex) const;
+
+        /**
+         * Overloaded compound multiplication operator for multiplying two complex numbers.
+         * 
+         * @param complex The complex number to multiply.
+         * @return The result of the multiplication operation.
+        */
+        Complex& operator*=(const Complex& complex);
+    };
+}
+
+#endif
